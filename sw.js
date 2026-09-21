01d706978c5e33a39757b2e783280ace9b65b744
Crisseu <cristi.amariutei_88@yahoo.com>
2026-09-21 21:17:31 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'istorie-v1';
+const assets = [
+  './',
+  './index.html',
+  './lectii.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
